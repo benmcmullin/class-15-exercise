b1052c343533b482dead8fe76e8fbfcb12d1e832
Ben McMullin <46494991+benmcmullin@users.noreply.github.com>
2022-10-07 14:23:13 -0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SearchResultsComponent
   ],
   imports: [
     BrowserModule
