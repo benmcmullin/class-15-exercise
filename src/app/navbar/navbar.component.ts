ee6ab44beda2a414005fcd7207690ce32c9cddcc
Ben McMullin <46494991+benmcmullin@users.noreply.github.com>
2022-10-19 12:09:44 -0500

    COMPLETED EXERCISE

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 name: string= '';
+search = [];
 
+public currentSearch() {
+  this.search.push(this.name);
+  this.name = '';
+  console.log(this.search)
+}
 
 
   constructor() { }
